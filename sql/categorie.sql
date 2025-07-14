db4358885dd704a3aa04ee233061a7f316903ee6
AnnickAndraina <andrainaannick@gmail.com>
2025-07-14 10:07:01 +0300

    sql base

--- sql/categorie.sql
+++ sql/categorie.sql
@@ -20,15 +20,35 @@ CREATE TABLE objet(
     id_categorie INT,
     id_membre INT,
     FOREIGN KEY (id_categorie) REFERENCES categorie_objet(id_categorie),
-    FOREIGN KEY (id_membre) REFERENCES membre(id_membre),
+    FOREIGN KEY (id_membre) REFERENCES membre(id_membre)
 );
 
 CREATE TABLE images_objet(
     id_image INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id_objet INT,
     nom_image VARCHAR(100),
-    FOREIGN KEY (id_objet) REFERENCES objet(id_objet),
+    FOREIGN KEY (id_objet) REFERENCES objet(id_objet)
 );
+
+CREATE TABLE emprunt (
+    id_emprunt INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_objet INT,
+    id_membre INT,
+    date_emprunt DATE,
+    date_retour DATE,
+    FOREIGN KEY (id_objet) REFERENCES objet(id_objet),
+    FOREIGN KEY (id_membre) REFERENCES membre(id_membre)
+);
+
+
+INSERT INTO membre (Nom , DateNaissance, Genre, Email, Ville, mdp, image_profil) VALUE
+('Koto', '2000-04-25', 'Homme', 'koto@gmail.com', 'Antananarivo', 'lalala', ''),
+('Bema', '1993-03-19', 'Femme', 'bema@gmail.com', 'Tamatave', 'oooo', ''),
+('Lola', '1983-12-31', 'Homme', 'lola@gmail.com', 'Mahajanga', 'eee1', ''),
+('Bozy', '2010-09-28', 'Femme', 'bozy@gmail.com', 'Fianarantsoa', 'eka301', '');
+
+INSERT INTO categorie_objet 
+
 
 
 
