c89bb714e42ea2049ee3f5045939a2ac629a4b0f
AnnickAndraina <andrainaannick@gmail.com>
2025-07-14 10:25:14 +0300

    basesql modif5

--- sql/categorie.sql
+++ sql/categorie.sql
@@ -41,16 +41,57 @@ CREATE TABLE emprunt (
 );
 
 
-INSERT INTO membre (Nom , DateNaissance, Genre, Email, Ville, mdp, image_profil) VALUE
+INSERT INTO membre (Nom , DateNaissance, Genre, Email, Ville, mdp, image_profil) VALUES
 ('Koto', '2000-04-25', 'Homme', 'koto@gmail.com', 'Antananarivo', 'lalala', ''),
 ('Bema', '1993-03-19', 'Femme', 'bema@gmail.com', 'Tamatave', 'oooo', ''),
 ('Lola', '1983-12-31', 'Homme', 'lola@gmail.com', 'Mahajanga', 'eee1', ''),
 ('Bozy', '2010-09-28', 'Femme', 'bozy@gmail.com', 'Fianarantsoa', 'eka301', '');
 
-INSERT INTO categorie_objet 
+INSERT INTO categorie_objet (nom_categorie) VALUES 
+('esthétique'),
+('bricolage'),
+('mécanique'),
+('mécanique');
 
-
-
-
-
+INSERT INTO objet (nom_objet,id_objet,id_membre) VALUES 
+('seche cheveux',1 , 1),
+('tournevis',2 , 1),
+('cle a molette',3 , 1),
+('mixeur',4 , 1),
+('fer a lisser',1 , 1),
+('marteau',2 , 1),
+('pompe a eau',3 , 1),
+('robot de cuisine',4 , 1),
+('peigne chauffant ',1 , 1),
+('perceuse',2 , 1),
+('seche cheveux',1 , 2),
+('tournevis',2 , 2),
+('cle a molette',3 , 2),
+('mixeur',4 , 2),
+('fer a lisser',1 , 2),
+('marteau',2 , 2),
+('pompe a eau',3 , 2),
+('robot de cuisine',4 , 2),
+('peigne chauffant ',1 , 2),
+('perceuse',2 , 2),
+('seche cheveux',1 , 3),
+('tournevis',2 , 3),
+('cle a molette',3 , 3),
+('mixeur',4 , 3),
+('fer a lisser',1 , 3),
+('marteau',2 , 3),
+('pompe a eau',3 , 3),
+('robot de cuisine',4 , 3),
+('peigne chauffant ',1 , 3),
+('perceuse',2 , 3),
+('seche cheveux',1 , 4),
+('tournevis',2 , 4),
+('cle a molette',3 , 4),
+('mixeur',4 , 4),
+('fer a lisser',1 , 4),
+('marteau',2 , 4),
+('pompe a eau',3 , 4),
+('robot de cuisine',4 , 4),
+('peigne chauffant ',1 , 4),
+('perceuse',2 , 4);
 
